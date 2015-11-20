b7747d4c386cf029c787904c193c10910248e46f
Pierre Lavaux <pierre.lvx@gmail.com>
2015-11-20 12:47:43 +0100

    Fixes + Travis

--- src/sdk_php/SQweb.php
+++ src/sdk_php/SQweb.php
@@ -9,33 +9,38 @@ class SQweb
      * @return int
      */
 
-    public function sqweb_check_credits() {
-        $site_id = null;
-        if (defined('ID_SITE')) {
-            $site_id = ID_SITE;
-        }
-        if (isset($_COOKIE['sqw_z']) && null !== $site_id) {
-            $cookiez = $_COOKIE['sqw_z'];
-        } else {
-            return (0);
-        }
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.sqweb.com/token/check',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_CONNECTTIMEOUT_MS => 1000,
-            CURLOPT_TIMEOUT_MS => 1000,
-            CURLOPT_POSTFIELDS => array(
-                'token' => $cookiez,
-                'site_id' => $site_id,
-            ),
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
+    private $response;
 
-        $response = json_decode($response);
-        if ($response != null && $response->status === true && $response->credit > 0) {
-            return ($response->credit);
+    public function sqweb_check_credits() {
+        if (empty($this->response)) {
+            $site_id = null;
+            if (defined('ID_SITE')) {
+                $site_id = ID_SITE;
+            }
+            if (isset($_COOKIE['sqw_z']) && null !== $site_id) {
+                $cookiez = $_COOKIE['sqw_z'];
+            } else {
+                return (0);
+            }
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.sqweb.com/token/check',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_CONNECTTIMEOUT_MS => 1000,
+                CURLOPT_TIMEOUT_MS => 1000,
+                CURLOPT_POSTFIELDS => array(
+                    'token' => $cookiez,
+                    'site_id' => $site_id,
+                ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+
+            $response = json_decode($response);
+            $this->response = $response;
+        }
+        if ($this->response != null && $this->response->status === true && $this->response->credit > 0) {
+            return ($this->response->credit);
         }
         return (0);
     }
